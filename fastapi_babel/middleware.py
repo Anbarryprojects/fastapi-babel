987a4d6faf247368de3a9bf1c36b89a0a95f693a
Fabian Beu <fabian.beu@adesso.de>
2023-05-22 18:01:44 +0200

    fixed getting language from header #19

--- fastapi_babel/middleware.py
+++ fastapi_babel/middleware.py
@@ -1,9 +1,11 @@
+import re
 from fastapi import Request, Response
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.middleware.base import RequestResponseEndpoint
 from starlette.middleware.base import DispatchFunction
 from starlette.types import ASGIApp
 from typing import TYPE_CHECKING, Optional
+from pathlib import Path
 
 if TYPE_CHECKING:
     from .core import Babel
@@ -19,6 +21,22 @@ class InternationalizationMiddleware(BaseHTTPMiddleware):
         super().__init__(app, dispatch)
         self.babel: "Babel" = babel
 
+    def get_language(self, lang_code):
+        languages = re.findall(r"([a-z]{2}-[A-Z]{2}|[a-z]{2})(;q=\d.\d{1,3})?", lang_code)
+        languages = sorted(languages, key=lambda x: x[1], reverse=True)
+        for lang in languages: # if language if path and no quantifier
+            if lang[0] in [i.name for i in list(Path(self.babel.config.BABEL_TRANSLATION_DIRECTORY).iterdir())] and not len(lang[1]):
+                return lang[0]
+
+
+
+        for lang in languages:
+            if lang[0] in [i.name for i in list(Path(self.babel.config.BABEL_TRANSLATION_DIRECTORY).iterdir())] and len(lang[1]):
+                return lang[0]
+
+        return self.babel.config.BABEL_DEFAULT_LOCALE
+
+
     async def dispatch(
         self, request: Request, call_next: RequestResponseEndpoint
     ) -> Response:
@@ -32,7 +50,7 @@ class InternationalizationMiddleware(BaseHTTPMiddleware):
             Response: ...
         """
         lang_code: Optional[str] = request.headers.get("Accept-Language", None)
-        if lang_code:
-            self.babel.locale = lang_code.split(",")[0]
+        self.babel.locale = self.get_language(lang_code)
+
         response: Response = await call_next(request)
         return response
