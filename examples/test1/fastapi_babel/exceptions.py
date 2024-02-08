7ebc63c631281dcb857ef10c3b362854b1fd1ca6
Legopapurida <papuridalego@gmail.com>
2024-02-08 14:20:37 +0330

    [FIX] test1.

--- examples/test1/fastapi_babel/exceptions.py
+++
@@ -1,4 +0,0 @@
-class BabelProxyError(Exception):
-    """When babel proxy object points to a None object will raise this error"""
-    def __init__(self, *args: object) -> None:
-        super().__init__("Proxy object points to an empty lookup instance")
