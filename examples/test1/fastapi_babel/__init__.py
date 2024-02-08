e0b9a81fdd8bdf6920bb179cf76f9af5a92ea7e6
Legopapurida <papuridalego@gmail.com>
2024-02-08 14:35:52 +0330

    Revert "[FIX] test1."
    This reverts commit 7ebc63c631281dcb857ef10c3b362854b1fd1ca6.

---
+++ examples/test1/fastapi_babel/__init__.py
@@ -0,0 +1,6 @@
+from .core import Babel, BabelCli, _
+from .properties import RootConfigs as BabelConfigs
+
+__version__ = "0.0.8"
+__author__ = "papuridalego@gmail.com"
+__all__ = ["Babel", "BabelCli", "BabelConfigs", "_"]
